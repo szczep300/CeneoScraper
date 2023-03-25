505fd1b3ee92f82b50fae9832aebbb2b3c014068
katwojuek <wojcikk@uek.krakow.pl>
2023-03-25 17:42:23 +0100

    2023-03-25 KW

---
+++ scraper.py
@@ -0,0 +1,7 @@
+import requests
+
+# product_code = input("Podaj kod produktu: ")
+product_code = "85920806"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = requests.get(url)
+print(response.status_code)
